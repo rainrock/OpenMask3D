8c8141d378a9110d9846cb63b3ffdc853fff1b20
Jan Ackermann <ackermannj@student.ethz.ch>
2023-05-26 16:47:02 +0200

    adds compute_clip_features

--- clip_features/clip_text_encoder.py
+++ clip_features/clip_text_encoder.py
@@ -1,39 +1,15 @@
 import os
 import numpy as np
 import clip
-import argparse
 
-def get_parser():
-    parser = argparse.ArgumentParser(description='DisNet')
-    parser.add_argument('--input', type=str)
-    parser.add_argument('--out_dir', type=str, default='./', help='specify the output directory')
-    args = parser.parse_args()
-    return args
-
-def main():
-    args = get_parser()
-    out_dir = args.out_dir
-    os.makedirs(out_dir, exist_ok=True)
-
-    # "ViT-L/14@336px" # the big model that OpenSeg uses
-    print('Loading the CLIP model...')
+def compute_clip_feature(text):
     clip_pretrained, _ = clip.load("ViT-L/14@336px", device='cpu', jit=False)
-    print('Finished loading.')
-    print('Ready for queries')
-
-    query_string = args.input
-
     # generate token
-    text = clip.tokenize([query_string])
+    text = clip.tokenize([text])
 
     text_features = clip_pretrained.encode_text(text)
 
     # normalize
     text_features = text_features / text_features.norm(dim=-1, keepdim=True)
 
-    # save features
-    np.save(os.path.join(out_dir, '{}.npy'.format(query_string)), text_features.detach().cpu().numpy())
-
-
-if __name__ == '__main__':
-    main()
+    text_features.detach().cpu().numpy()
