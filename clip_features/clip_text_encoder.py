835df909fc3baefd68b9e208bcca0feaa77697f1
Jan Ackermann <ackermannj@student.ethz.ch>
2023-05-26 18:08:04 +0200

    adds compute_clip_distance function

--- clip_features/clip_text_encoder.py
+++ clip_features/clip_text_encoder.py
@@ -1,5 +1,6 @@
 
 import clip
+import numpy as np
 
 def compute_clip_feature(text):
     clip_pretrained, _ = clip.load("ViT-L/14@336px", device='cpu', jit=False)
@@ -12,3 +13,7 @@ def compute_clip_feature(text):
     text_features = text_features / text_features.norm(dim=-1, keepdim=True)
 
     return text_features.detach().cpu().numpy()
+
+
+def compute_clip_distance(feature1, feature2):
+    return np.dot(feature1, feature2)/(np.linalg.norm(feature1) * np.linalg.norm(feature2))
