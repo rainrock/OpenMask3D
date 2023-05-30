18ba84576b524f4978dec45f24ab68ba16027ab8
keli <keli@student.ethz.ch>
2023-05-30 13:39:08 +0200

    read

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
 import argparse
 import subprocess
 import os
+import numpy as np
 from fusion import feature_fusion
 
 def main(args):
     # Your code logic goes here
     print(f"Input file: {args.input}")
-    filename = os.path.basename(args.input)
     
     if args.verbose:
         print("Verbose mode enabled")
 
     # Call Adams thingy here
     # TODO @Adam
-    p0 = subprocess.Popen([["python3", "mask3d.py"]])
+    processed_mask3d = subprocess.Popen([["python3", "mask3d.py"]])
 
-    # Do preprocessing
-    p1 = subprocess.Popen(["python3", "preprocess/preprocess_3d_replica.py"])
-    p2 = subprocess.Popen(["python3", "preprocess/preprocess_2d_replica.py"])
+    # # Do preprocessing
+    # p1 = subprocess.Popen(["python3", "preprocess/preprocess_3d_replica.py"])
+    # p2 = subprocess.Popen(["python3", "preprocess/preprocess_2d_replica.py"])
 
-    exit_codes = [p.wait() for p in [p1, p2]]
+    # exit_codes = [p.wait() for p in [p1, p2]]
 
-    # Get Clip features
-    p3 = subprocess.Popen(["python3", "clip_features/replica_openseg.py"])
+    # # Get Clip features
+    # p3 = subprocess.Popen(["python3", "clip_features/replica_openseg.py"])
 
-    exit_codes = [p.wait() for p in [p0, p3]]
+    # exit_codes = [p.wait() for p in [p0, p3]]
 
     # Compute clip feature per instance prediction    
-    clip_feature_per_instance = feature_fusion(p0, p3)
+    
+    # read precomputed clip feature
+    point_cloud_clip_feature = np.load(args.clip_feature_path)
+    
+    # compute clip feature per instance
+    clip_feature_per_instance = feature_fusion(processed_mask3d, point_cloud_clip_feature)
     
     query = "" # get the query somehow
     # Compute the feature for the query
 
-    p4 = subprocess.Popen(["python3", "clip_features/clip_text_encoder.py", "--input", "{}".format(query)])
+    #p4 = subprocess.Popen(["python3", "clip_features/clip_text_encoder.py", "--input", "{}".format(query)])
 
     # output
 
@@ -43,7 +48,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Your program description")
 
     # Add arguments to the parser
-    parser.add_argument("input", help="Input file path")
+    parser.add_argument("clip_feature_path", help="Input file path")
     parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose mode")
 
     # Parse the command-line arguments
